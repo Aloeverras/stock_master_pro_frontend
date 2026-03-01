e574b2bc6e648c895184bdc0bff0dbd4af656bd3
Aloeverras <edouard.bouche@gmail.com>
2026-03-01 21:31:21 +0000

    init StockBadge tsx

--- src/components/StockBadge.tsx
+++ src/components/StockBadge.tsx
@@ -1,19 +1,27 @@
-import React from "react";
+import "../scss/StockerBadge.scss"
+import React, { useEffect, useState } from "react";
 import { StockBadgeProps } from "../interfaces/StockBadgeProps";
 import { StockStatusType } from "../types/StockStatusType";
 
 const StockBadge : React.FC<StockBadgeProps> = ({quantity, threshold}) => {
 
-    let status : StockStatusType = "IN_STOCK";
+    // state hook
+    const [status, setStatus] = useState<StockStatusType>("IN_STOCK");
+    const [colorClass, setColorClass] = useState<string>("bg-green-100 text-green-800")
 
-    if (quantity <= 0) {
-        status = "OUT_OF_STOCK";
-    } else if (quantity <= threshold) {
-        status = 'LOW_SCTOCK'
-    };
+    useEffect(() : void => {
+        if (quantity <= 0) {
+            setStatus("OUT_OF_STOCK");
+        } else if (quantity <= threshold) {
+            setStatus("LOW_SCTOCK");
+            setColorClass("bg-orange-100 text-orange-100");
+        } else {
+            console.log("IN_STOCK");
+        };
+    }, [status, colorClass])
 
     return (
-        <span>
+        <span className={`stockageBadge ${colorClass}`}>
             {status.replace('_', ' ')}
         </span>
     );
