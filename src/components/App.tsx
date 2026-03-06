818ade663ce353106cb4f8277006564f516ef344
Aloeverras <edouard.bouche@gmail.com>
2026-03-06 15:43:14 +0000

    retry

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,13 @@
-import "../scss/App.scss";
 import React  from "react";
 import HeaderApp  from "./HeaderApp"
-
-type HeaderType = typeof HeaderApp;
-
-const Header : HeaderType = HeaderApp;
 
 const App : React.FC = 
 
     () : React.JSX.Element => {
 
-
-        
-
         return (
             <div id="app-container">
-                <Header></Header>
+                <HeaderApp></HeaderApp>
                 <main></main>
                 <footer></footer>
             </div>
