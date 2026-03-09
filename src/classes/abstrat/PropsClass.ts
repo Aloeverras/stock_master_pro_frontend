8c49a3b9ba0bc3134f2e6c1c0246e0bb95189a66
Aloeverras <edouard.bouche@gmail.com>
2026-03-09 07:13:23 +0000

    update PropsClass

--- src/classes/abstrat/PropsClass.ts
+++ src/classes/abstrat/PropsClass.ts
@@ -1,36 +1,60 @@
-export default abstract class PropsClass<T> {
+import { CSSProperties, ReactNode } from "react";
 
-    protected props : T;
-    protected id : string | undefined
-    protected classNames : string | undefined;
+export default abstract class PropsClass<T> {
+    protected id? : string;
+    protected className? : string;
+    protected style? : CSSProperties;
+    protected children? : ReactNode;
+    protected data? : T;
+    protected isLoading?: boolean;
+    protected isDisabled?: boolean;
 
     constructor(
-        props : T,
-        id : string | undefined = undefined,
-        classNames : string | undefined = undefined
-    ) {
-        this.props = props;
+        data : T,
+        id : string  = "",
+        className : string = "",
+        style : CSSProperties | undefined = undefined,
+        children : ReactNode | undefined = undefined,
+        isLoading : boolean | undefined = undefined,
+        isDisabled : boolean | undefined = undefined
+
+    ){
+        this.data = data;
         this.id = id;
-        this.classNames = classNames;
+        this.className = className;
+        this.style = style;
+        this.children = children;
+        this.isLoading = isLoading;
+        this.isDisabled = isDisabled;
     };
 
-    public get getProps() : T {
-        return this.props;
+    // getters
+
+    public get getData() : T | undefined {
+        return this.data;
     };
 
-    public get getIdValue() : string | undefined {
+    public get getIdProd() : string | undefined {
         return this.id;
     };
 
-    public get getClassNames() : string | undefined {
-        return this.classNames;
+    public get getClassNameProp() : string | undefined {
+        return this.className;
     };
 
-    public set setClassNames(newClassName : string ) {
-        this.classNames = newClassName;
+    public get getStyleProps() : CSSProperties | undefined {
+        return this.style;
+    };
+
+    public get getChildrenProp() : ReactNode | undefined {
+        return this.children;
+    };
+
+    public get getIsLoading() : boolean | undefined {
+        return this.isLoading;
+    };
+
+    public get getIsDisabled() : boolean | undefined {
+        return this.isDisabled;
     }
-    
-    // absrat functions
-    abstract toDictProp() : T;
-    abstract toListKeysProps() : string[];
 };
